755387db921d5a845f9abbf03a3141737fac1d03
guidiamond <guilherme.batista770@gmail.com>
2021-05-06 21:45:18 -0300

    memoriaRom with desired instructions

--- components/memoriaRom.vhd
+++ components/memoriaRom.vhd
@@ -7,18 +7,27 @@ entity memoriaRom IS
           dataWidth: natural := 32;
           addrWidth: natural := 32;
        memoryAddrWidth:  natural := 6 );   -- 64 posicoes de 32 bits cada
-   port ( clk      : IN  STD_LOGIC;
-          Endereco : IN  STD_LOGIC_VECTOR (addrWidth-1 DOWNTO 0);
+   port ( Endereco : IN  STD_LOGIC_VECTOR (addrWidth-1 DOWNTO 0);
           Dado     : OUT STD_LOGIC_VECTOR (dataWidth-1 DOWNTO 0) );
 end entity;
 
 architecture assincrona OF memoriaRom IS
   type blocoMemoria IS ARRAY(0 TO 2**memoryAddrWidth - 1) OF std_logic_vector(dataWidth-1 DOWNTO 0);
 
-  signal memROM: blocoMemoria;
-  attribute ram_init_file : string;
-  attribute ram_init_file of memROM:
-  signal is "ROMcontent.mif";
+  -- signal memROM: blocoMemoria;
+  -- attribute ram_init_file : string;
+  -- attribute ram_init_file of memROM:
+  -- signal is "ROMcontent.mif";
+  FUNCTION initMemory
+    RETURN blocoMemoria IS VARIABLE tmp : blocoMemoria := (OTHERS => (OTHERS => '0'));
+  BEGIN
+     
+    tmp(1) := x"02324022"; -- sub $t0, $s1, $s2
+    tmp(2) := x"02324022"; -- add $t0, $s1, $s2
+    RETURN tmp;
+  END initMemory;
+
+  SIGNAL memROM : blocoMemoria := initMemory;
 
 -- Utiliza uma quantidade menor de endereços locais:
    signal EnderecoLocal : std_logic_vector(memoryAddrWidth-1 downto 0);
@@ -27,3 +36,4 @@ begin
   EnderecoLocal <= Endereco(memoryAddrWidth+1 downto 2);
   Dado <= memROM (to_integer(unsigned(EnderecoLocal)));
 end architecture;
+
--- main.vhd
+++ main.vhd
@@ -6,7 +6,8 @@ entity main is
   generic (
             DATA_WIDTH : NATURAL := 32;
             ADDR_WIDTH : NATURAL := 32;
-            CONSTANTE_PC: NATURAL := 4
+            CONSTANTE_PC: NATURAL := 4;
+            REG_WIDTH: NATURAL := 5
           );
 
   port
@@ -22,17 +23,17 @@ architecture arch_name of main is
   signal saidaPC, saidaSomaUm : std_logic_vector(ADDR_WIDTH-1 downto 0);
   signal saidaULA : std_logic_vector(DATA_WIDTH-1 downto 0);
 
-  signal flagZero : std_logic; -- Ocorre se EntradaUlaA == EntradaUlaB
+  -- signal flagZero : std_logic; -- Ocorre se EntradaUlaA == EntradaUlaB 
   signal instrucaoRom : std_logic_vector(DATA_WIDTH-1 downto 0);
 
   signal saidaRegA : std_logic_vector(DATA_WIDTH-1 downto 0);
   signal saidaRegB : std_logic_vector(DATA_WIDTH-1 downto 0);
 
   -- Instrucao alias
-  alias opCodeIn : std_logic_vector(ADDR_WIDTH-1 downto 26) is instrucaoRom(ADDR_WIDTH-1 downto 26);
-  alias imediatoRs : std_logic_vector(25 downto 21) is instrucaoRom(25 downto 21);
-  alias imediatoRt : std_logic_vector(20 downto 16) is instrucaoRom(20 downto 16);
-  alias imediatoRd : std_logic_vector(15 downto 11) is instrucaoRom(15 downto 11);
+  alias opCodeIn   : std_logic_vector(5 downto 0) is instrucaoRom(ADDR_WIDTH-1 downto 26);
+  alias imediatoRs : std_logic_vector(REG_WIDTH-1 downto 0) is instrucaoRom(25 downto 21);
+  alias imediatoRt : std_logic_vector(REG_WIDTH-1 downto 0) is instrucaoRom(20 downto 16);
+  alias imediatoRd : std_logic_vector(REG_WIDTH-1 downto 0) is instrucaoRom(15 downto 11);
 
 begin
 
@@ -43,7 +44,7 @@ begin
     port map(entrada => saidaPC, saida => saidaSomaUm);
 
   ROM: entity work.memoriaRom
-    port map(clk => Clk, Endereco => SaidaPC, Dado => instrucaoRom);
+    port map(Endereco => SaidaPC, Dado => instrucaoRom);
 
   BancoRegistradores: entity work.bancoRegistradores generic map (larguraDados => DATA_WIDTH, larguraEndBancoRegs => 5)
     port map (
@@ -52,7 +53,7 @@ begin
               enderecoB => imediatoRt,
               enderecoC => imediatoRd,
               dadoEscritaC => saidaULA,
-              escreveC => escritaC, -- hardcoded enquanto o UC não estiver impl
+              escreveC => escritaC,
               saidaA => saidaRegA,
               saidaB => saidaRegB
             );
@@ -62,9 +63,9 @@ begin
     port map (
                entradaA => saidaRegA,
                entradaB => saidaRegB,
-               seletor => operacaoULA, -- Seletor 000 hardcoded para testar soma (já que o UC não está implementado)
-               saida => saidaULA,
-               flagZero => flagZero
+               seletor => operacaoULA,
+               saida => saidaULA
+               -- flagZero => flagZero
              ); 
 
 end architecture;
