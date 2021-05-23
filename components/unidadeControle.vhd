b51f035d18d2208b74335ada3a70cf5f6be2740b
guidiamond <guilherme.batista770@gmail.com>
2021-05-23 14:57:44 -0300

    unidade de controle feita

--- components/unidadeControle.vhd
+++ components/unidadeControle.vhd
@@ -1,50 +1,63 @@
--- 100011 (lw)
--- 101011 (sw)
--- 000100 (beq)
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity unidadeControle is
   generic (
-              OPCODE_WIDTH: natural := 6;
-              PALAVRA_CONTROLE_WIDTH: natural := 6
-  );
-  port   (
-    -- Input ports
-    clk  :  in  std_logic;
-    opCode  :  in  std_logic_vector(OPCODE_WIDTH-1 downto 0);
-    -- Output ports
-    palavraControle  :  out std_logic_vector(PALAVRA_CONTROLE_WIDTH-1 downto 0)
-  );
+            OPCODE_WIDTH: natural := 6;
+            PALAVRA_CONTROLE_WIDTH: natural := 6
+          );
+  port (
+         -- Input ports
+         clk  :  in  std_logic;
+         opCode  :  in  std_logic_vector(OPCODE_WIDTH-1 downto 0);
+         -- Output ports
+         palavraControle  :  out std_logic_vector(PALAVRA_CONTROLE_WIDTH-1 downto 0)
+       );
 end entity;
-
 
 architecture arch_name of unidadeControle is
   signal pontosControle : std_logic_vector(PALAVRA_CONTROLE_WIDTH-1 downto 0);
 
   -- Output alias (mais facil de atribuir os valores)
-  alias habEscritaReg : std_logic is pontosControle(0);
-  alias selOperacaoULA : std_logic_vector(2 downto 0) is pontosControle(3 downto 1);
-  alias habEscritRam : std_logic is pontosControle(4);
-  alias habLeituraRam : std_logic is pontosControle(5);
+  alias mux_PcBeq_J   : std_logic is pontosControle(0);
+  alias mux_rt_rd     : std_logic is pontosControle(1);
+  alias habEscritaReg : std_logic is pontosControle(2);
+  alias mux_rt_imed   : std_logic is pontosControle(3);
+  alias mux_ula_mem   : std_logic is pontosControle(4);
+  alias beqUC           : std_logic is pontosControle(5);
+  alias habEscritRam  : std_logic is pontosControle(6);
+  alias habLeituraRam : std_logic is pontosControle(7);
+  alias ulaOP         : std_logic_vector(2 downto 0) is pontosControle(10 downto 8);
 
   -- INSTRUCTIONS
-  constant lw : std_logic_vector := "100011";
-  constant sw  : std_logic_vector := "101011";
-  constant beq   : std_logic_vector := "000100";
+  constant instrucaoR : std_logic_vector := "000000"; -- Funct define operação
+  constant instrucaoJ : std_logic_vector := "000010";
+  -- Tipo I
+  constant lw   : std_logic_vector := "100011";
+  constant sw   : std_logic_vector := "101011";
+  constant beq  : std_logic_vector := "000100";
 
   begin
-      habEscritaReg <= '1' when opCode = lw else '0';
+    mux_PcBeq_J <= '1' when opCode = instrucaoJ else '0';
 
-    selOperacaoULA <= "000" when opCode = lw or opCode = sw else
-                   "000";
+    mux_rt_imed <= '1' when opCode = instrucaoR;
 
-    habEscritRam <= '1' when opCode = sw else '0';
+    habEscritaReg <= '1' when (opCode = instrucaoR or opCode = lw) else '0';
+
+    mux_rt_imed <= '1' when (opCode = lw or opCode = sw) else '0';
+
+    mux_ula_mem <= '1' when opCode = lw else '0';
+
+    beqUC <= '1' when opCode = beq else '0';
 
     habLeituraRam <= '1' when opCode = lw else '0';
 
+    habEscritRam <= '1' when opCode = sw else '0';
+
+    ulaOP <= "10" when opCode = instrucaoR else
+             "01" when opCode = beq else
+             "00";
 
     -- Assign resultado final para output
     palavraControle <= pontosControle;
